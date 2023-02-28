6b33fe2aaff4439dc48275faeeef6e9854b4438f
yukishi24 <yukishi24github@gmail.com>
2023-02-28 21:59:21 +0900

    modify: FizzBuzz.py

--- src/FizzBuzz.py
+++ src/FizzBuzz.py
@@ -7,5 +7,9 @@ for i in range(max):
     num=i+1;
     if num%3==0 and num%5==0:
         print(str(num) +" " + "FizzBuzz");
+    elif num%3==0 and num%5!=0:
+        print(str(num) + " " + "Fizz");
+    elif num%3!=0 and num%5==0:
+        print(str(num) + " " + "Buzz");
     else:
         print(num);

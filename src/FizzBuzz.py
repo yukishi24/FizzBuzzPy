cff4360ad57d766e4bcb248e50cf360188b9ec1d
yukishi24 <yukishi24github@gmail.com>
2023-02-28 21:55:25 +0900

    modify: FizzBuzz.py

--- src/FizzBuzz.py
+++ src/FizzBuzz.py
@@ -1,8 +1,11 @@
 # 入力値を変数maxに格納。
 tmp=input("最大値を入力してください。>>>");
 print("==============================");
-# 出力
 max=int(tmp);
 
 for i in range(max):
-    print(i+1);
+    num=i+1;
+    if num%3==0 and num%5==0:
+        print(str(num) +" " + "FizzBuzz");
+    else:
+        print(num);
